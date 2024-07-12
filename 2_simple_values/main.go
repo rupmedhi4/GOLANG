e3e7331b16ff7513f3598815cc3ca66da581422e
Rup medhi <rupmmedhi@gmail.com>
2024-07-12 22:20:40 +0530

    variables

---
+++ 2_simple_values/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//simple values
+	fmt.Println(1) // int
+	fmt.Println("hello golang") // string
+	fmt.Println(true) // bool
+	fmt.Println(10.5) // float64
+}
---
+++ 3_variables/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var name string = "golang"
+
+	//infer
+	//var name = "golang"
+
+	//shorthand syntax
+	//name :="golang"
+
+	var name string
+	name = "golang"
+
+	fmt.Println(name)
+} 
