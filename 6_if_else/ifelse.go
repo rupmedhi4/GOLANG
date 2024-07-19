63ac5d5bdb9b2c184557a2d108ada0f4fa05376e
Rup medhi <rupmmedhi@gmail.com>
2024-07-20 00:05:17 +0530

    if else

---
+++ 6_if_else/ifelse.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	// age := 16
+
+	// if age >= 18 {
+	// 	fmt.Println("person is an adult")
+	// }else{
+	// 	fmt.Println("person is a minor")
+	// }
+
+
+	// age := 16
+
+	// if age >=18{
+	// 	fmt.Println("person is an adult")
+	// }else if age >=12 {
+	// 	fmt.Println("person is a teenager")
+	// }else{
+	// 	fmt.Println("person is a kid")
+	// }
+
+
+	// var role ="admin"
+	// var hasPermissions = true
+
+	// if role == "admin" || hasPermissions{
+	// 	fmt.Println("user is an admin")
+	// }
+
+// we can declare a veriable inside if construct
+	// if age :=15; age >=18{
+	// 	fmt.Println("person is an adult")
+	// }else if age >= 12{
+	// 	fmt.Println("person is a teenager")
+	// }
+
+
+
+	//go does not have ternary operator, you will have to use normal if else condition
+
+
+
+
+}
