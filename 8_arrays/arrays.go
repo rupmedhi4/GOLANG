aabe9080bd09aba7030b5427a6ee8549640214ff
Rup medhi <rupmmedhi@gmail.com>
2024-07-23 23:19:43 +0530

    arrays

---
+++ 8_arrays/arrays.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var nums [4]int
+
+	//int ->0,string->"",bool->false
+
+	// nums[0]=1
+	// fmt.Println((nums[0]))
+	// fmt.Println((nums)
+
+	//array length
+	//fmt.Println(len(nums))
+
+
+	// var vals [4]bool
+	// vals[2]= true
+	// fmt.Println(vals)
+
+
+	// var name [3]string
+	// name[0] ="rup"
+	// fmt.Println(name)
+
+	// nums :=[3]int{1,2,3}
+	// fmt.Println(nums)
+
+	//2d array
+	nums :=[2][2]int{{3,4},{5,6}}
+	fmt.Println(nums)
+
+
+
+	// fixed size, that is predictable
+	// memory optimization
+	//constant time access
+}
